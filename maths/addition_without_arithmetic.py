7666bca219ac7d6baccd0198ff801721987786b4
edurochasi <139974228+edurochasi@users.noreply.github.com>
2023-07-28 11:33:51 +0100

    Update addition_without_arithmetic.py

--- maths/addition_without_arithmetic.py
+++ maths/addition_without_arithmetic.py
@@ -4,21 +4,7 @@ Illustrate how to add the integer without arithmetic operation
 
 
 def add(first: int, second: int) -> int:
-    """
-    Implementation of addition of integer
 
-    Examples:
-    >>> add(3, 5)
-    8
-    >>> add(13, 5)
-    18
-    >>> add(-7, 2)
-    -5
-    >>> add(0, -7)
-    -7
-    >>> add(-321, 0)
-    -321
-    """
     while second != 0:
         c = first & second
         first ^= second
