ea051cc8fc5adbecd898997606b3a43d113e4616
edurochasi <139974228+edurochasi@users.noreply.github.com>
2023-08-08 15:19:36 +0100

    Update allocation_number.py

--- maths/allocation_number.py
+++ maths/allocation_number.py
@@ -7,40 +7,26 @@ For example:
 """
 from __future__ import annotations
 
+class Mathing:
+    def __init__(self, number_of_bytes, partitions):
+        self.number_of_bytes = number_of_bytes
+        self.partitions = partitions
 
-def allocation_num(number_of_bytes: int, partitions: int) -> list[str]:
-    """
-    Divide a number of bytes into x partitions.
-    :param number_of_bytes: the total of bytes.
-    :param partitions: the number of partition need to be allocated.
-    :return: list of bytes to be assigned to each worker thread
+    def allocation_num(self) -> list[str]:
 
-    >>> allocation_num(16647, 4)
-    ['1-4161', '4162-8322', '8323-12483', '12484-16647']
-    >>> allocation_num(50000, 5)
-    ['1-10000', '10001-20000', '20001-30000', '30001-40000', '40001-50000']
-    >>> allocation_num(888, 999)
-    Traceback (most recent call last):
-        ...
-    ValueError: partitions can not > number_of_bytes!
-    >>> allocation_num(888, -4)
-    Traceback (most recent call last):
-        ...
-    ValueError: partitions must be a positive number!
-    """
-    if partitions <= 0:
-        raise ValueError("partitions must be a positive number!")
-    if partitions > number_of_bytes:
-        raise ValueError("partitions can not > number_of_bytes!")
-    bytes_per_partition = number_of_bytes // partitions
-    allocation_list = []
-    for i in range(partitions):
-        start_bytes = i * bytes_per_partition + 1
-        end_bytes = (
-            number_of_bytes if i == partitions - 1 else (i + 1) * bytes_per_partition
-        )
-        allocation_list.append(f"{start_bytes}-{end_bytes}")
-    return allocation_list
+        if self.partitions <= 0:
+            raise ValueError("partitions must be a positive number!")
+        if self.partitions > self.number_of_bytes:
+            raise ValueError("partitions can not > number_of_bytes!")
+        bytes_per_partition = self.number_of_bytes // self.partitions
+        allocation_list = []
+        for i in range(self.partitions):
+            start_bytes = i * bytes_per_partition + 1
+            end_bytes = (
+                self.number_of_bytes if i == self.partitions - 1 else (i + 1) * bytes_per_partition
+            )
+            allocation_list.append(f"{start_bytes}-{end_bytes}")
+        return allocation_list
 
 
 if __name__ == "__main__":
