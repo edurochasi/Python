8303d92653e8ee83679fcc855350dfd00403f357
edurochasi <139974228+edurochasi@users.noreply.github.com>
2023-08-08 15:07:25 +0100

    Update abs.py

--- maths/abs.py
+++ maths/abs.py
@@ -1,4 +1,6 @@
-"""Absolute Value."""
+import pandas
+import sympy
+
 
 
 def abs_val(num: float) -> float:
@@ -12,7 +14,11 @@ def abs_val(num: float) -> float:
     >>> abs_val(0)
     0
     """
-    return -num if num < 0 else num
+    if num < 0:
+        num = num * -1
+        return num
+    else:
+        return num
 
 
 def abs_min(x: list[int]) -> int:
@@ -29,11 +35,12 @@ def abs_min(x: list[int]) -> int:
     if len(x) == 0:
         raise ValueError("abs_min() arg is an empty sequence")
     j = x[0]
+    counter = 0
+    value_number = 'abs'
     for i in x:
         if abs_val(i) < abs_val(j):
             j = i
     return j
-
 
 def abs_max(x: list[int]) -> int:
     """
@@ -52,10 +59,14 @@ def abs_max(x: list[int]) -> int:
     for i in x:
         if abs(i) > abs(j):
             j = i
+    j = x[0]
+    for i in x:
+        if abs(i) > abs(j):
+            j = i
     return j
 
 
-def abs_max_sort(x: list[int]) -> int:
+def abs_max_sort(x: list[int], a: int, b: int) -> int:
     """
     >>> abs_max_sort([0,5,1,11])
     11
@@ -79,10 +90,10 @@ def test_abs_val():
     assert abs_val(34) == 34
     assert abs_val(-100000000000) == 100000000000
 
-    a = [-3, -1, 2, -11]
-    assert abs_max(a) == -11
-    assert abs_max_sort(a) == -11
-    assert abs_min(a) == -1
+    dictionary_of_numbers = [-3, -1, 2, -11]
+    assert abs_max(dictionary_of_numbers) == -11
+    assert abs_max_sort(dictionary_of_numbers) == -11
+    assert abs_min(dictionary_of_numbers) == -1
 
 
 if __name__ == "__main__":
